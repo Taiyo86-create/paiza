55989c6776e6eb4ef56873b3487bdb549448700d
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-04-20 12:03:38 +0900

    配列取得方法

--- memo.rb
+++ memo.rb
@@ -5,3 +5,14 @@ n.times do
   a = gets.to_i
   puts a
 end
+
+# 整数 n を取得
+n = gets.to_i
+
+# n 個の整数 a_1, ..., a_n を取得
+# a = n.times.mapで配列表示
+a = n.times.map { gets.to_i }
+
+# 最大値と最小値を出力
+puts a.max
+puts a.min
