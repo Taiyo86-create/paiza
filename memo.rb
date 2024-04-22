84fe5cfec07946cb6e60c8b3bdb2090f7701709f
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-04-22 11:34:10 +0900

    Update memo.rb

--- memo.rb
+++ memo.rb
@@ -16,3 +16,39 @@ a = n.times.map { gets.to_i }
 # 最大値と最小値を出力
 puts a.max
 puts a.min
+
+# 半角スペースで横に表示
+n = gets.to_i
+numbers = gets.split.map(&:to_i)
+puts numbers
+
+# 複数の値の取得
+n = gets.to_i
+
+# 各社員の情報を処理
+n.times do
+    # 社員の名前と昨年度の年齢を半角スペースで区切って受け取る
+    name, age = gets.split
+
+    # 今年度の年齢を計算して出力
+    puts "#{name} #{age.to_i + 1}"
+end
+
+# あなたの食べたい料理名を受け取る
+s = gets.chomp
+
+# 与えられるメニューの単語数を受け取る
+n = gets.to_i
+
+# メニュー名を受け取る
+menu = gets.chomp.split
+
+# あなたの食べたい料理がメニューに含まれているかチェック
+if menu.include?(s)
+  puts "Yes"
+else
+  puts "No"
+end
+
+# 整数方を文字列に変換
+puts a.to_s.length
